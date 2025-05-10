2cc6c67968606d4be5bd0434977c8d4e16ccc658
hrsh-1294 <harshvashishth12@gmail.com>
2025-05-10 21:11:49 +0530

    added browser router

--- SnippetStash/src/main.jsx
+++ SnippetStash/src/main.jsx
@@ -5,12 +5,15 @@ import App from './App.jsx'
 import { store } from './store.js'
 import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast';
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
       <App />
       <Toaster />
+      </BrowserRouter>
     </Provider>
   </StrictMode>,
 )
