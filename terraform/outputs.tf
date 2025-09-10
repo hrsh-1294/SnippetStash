9bebd11f682bf11d5a975cad039481035b070602
hrsh-1294 <harshvashishth12@gmail.com>
2025-09-10 23:09:11 +0530

    removed outputs.tf

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.snippetstash_server.public_ip
-}
-
-output "public_dns" {
-  value = aws_instance.snippetstash_server.public_dns
-}
