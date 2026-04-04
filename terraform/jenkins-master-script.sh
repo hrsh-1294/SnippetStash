6f288829487f06dd095c317bbeea4e50bb99f3ff
hrsh-1294 <harshvashishth12@gmail.com>
2026-04-04 17:05:45 +0530

    changed installation script

--- terraform/jenkins-master-script.sh
+++ terraform/jenkins-master-script.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
-#install java
+
+# Install Java
 sudo apt update -y
 sudo apt install -y fontconfig openjdk-21-jre
 java -version
 
-#install jenkins
+# FIX: create keyrings directory
+sudo mkdir -p /etc/apt/keyrings
+
+# Install Jenkins
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+  sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
 sudo apt-get update -y
+
+# Install Jenkins
 sudo apt-get install -y jenkins
 
-systemctl status jenkins
-sudo systemctl enable jenkins
-sudo systemctl restart jenkins
+# Reload systemd (VERY IMPORTANT)
+sudo systemctl daemon-reload
 
+# Enable + start Jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Check status
+sudo systemctl status jenkins
